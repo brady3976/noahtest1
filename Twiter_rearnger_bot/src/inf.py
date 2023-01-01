a2deb208d321fe0a35bb48e7427b9dc1286f9f19
brady3976 <brady3976@yahoo.com>
2023-01-01 15:03:38 -0500

    Add files via upload

---
+++ Twiter_rearnger_bot/src/inf.py
@@ -0,0 +1,48 @@
+class env:
+
+    user_list = []
+
+
+
+
+
+
+
+    API_KEY = 'LPCVPQxcISIlwc4u9cbc5npJB' # This var saves the API KEY 
+    API_KEY_SECRET = 'Xnt8w99mQKEsUwbi5cylJ5gl2JIeMBjsF5nC6dTYMlrIIk8MgB' # This var saves the Secret key 
+    ACCESS_TOKEN = '1325831661582479363-TnvjKhVj62f7dAvZllkwJyeoWpgnNC'
+    ACCESS_TOKEN_SECRET = 'AVGSH2ZhFhIMlSprEe7wjsHyNe1uIv8mNiF3kX9Mdk5Lc'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
